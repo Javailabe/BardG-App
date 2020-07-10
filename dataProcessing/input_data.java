5ff28a768b3c8b398780feda36dcc241a2398fba
Javailabe <kulecki.pawel@gmail.com>
2020-07-10 21:53:19 +0200

    Update glownego menu oraz proba wprowadzenia metody dodajacej uzytkownika do listy

--- dataProcessing/input_data.java
+++ dataProcessing/input_data.java
@@ -1,4 +1,34 @@
 package dataProcessing;
 
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class input_data {
+    public static void input(String args[])
+    {
+        String wpis;
+        BufferedReader dodaj = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Dodaj uzytkownika wedlug ponizszego schematu lub uzyj (.) by zakonczyc: ");
+        System.out.println("Imie, Nazwisko, email");
+        System.out.println();
+
+        try (FileWriter fw = new FileWriter("D:/Java/BoardG-App/data base/users.txt", true))
+        {
+            do {
+                System.out.print("Dodaj uzytkownika lub wybierz (.) by zakonczyc: ");
+                wpis = dodaj.readLine();
+                if(wpis.compareTo(".") == 0) break;
+
+                wpis = wpis +"\r\n"; //rozpoczecie nowego wiersza
+                fw.write(wpis);
+            } while(wpis.compareTo(".") != 0);
+        } catch(IOException exc) {
+            System.out.println("Blad wejscia-wyjscia: " + exc)
+        }
+
+
+    }
 }
--- main_menu.java
+++ main_menu.java
@@ -11,34 +11,36 @@ public class main_menu {
 
         int mainChoice, submainChoice;
 
-        //czesc informacyjna
+        //menu glowne
+        System.out.println();
         System.out.println("Witaj w programie BoardG-App!");
         System.out.println();
         System.out.println("Wybierz jedna z ponizszych opcji aby wykonac rzadana operacje a nastepnie nacisnij ENTER:");
-        System.out.println("        Wybierz (1) - aby zobaczyc baze danych");
-        System.out.println("        Wybierz (2) - dodac wpis do bazy danych");
+        System.out.println();
+        System.out.println("        Wybierz (1) - by podejrzec liste gier/uzytkoniwkow");
+        System.out.println("        Wybierz (2) - by dodac wpis do listy gier/uzytkownikow");
         System.out.println("        Wybierz (0) - aby zakonczyc dzialanie programu");
         System.out.println();
 
-        //tutaj dokonujemy wyboru oraz sprawdzamy czy wybrana opcja jest wyborem zgodnym z wytycznymi
-
-            System.out.print("        Wybieram: ");
+        //wybor uzytkoniwka
+        System.out.print("        Wybieram: ");
             mainChoice = (char) System.in.read();
 
             //koniec programu
             if (mainChoice == '0') {
-                System.out.print("--Konczenie dziala programu...");
+                System.out.print("--Konczenie dzialania programu...");
             }
 
             //Podglad bazy danych
             else if (mainChoice == '1') {
+                System.out.println();
                 System.out.println("Podaj baze danych ktora chcesz wywolac:");
-                System.out.println("    Wybierz (1) - baza gier");
-                System.out.println("    Wybierz (2) - baza uzytkownikow");
+                System.out.println("    Wybierz (1) - lista gier");
+                System.out.println("    Wybierz (2) - lista uzytkownikow");
                 System.out.println("    Wybierz (0) - by wrocic do poprzedniego menu");
                 System.out.println();
 
-                System.out.print("      Wybieram: ");
+                System.out.print("    Wybieram: ");
                 submainChoice = (char) System.in.read();
 
                             if (submainChoice == '0' ) {
@@ -46,19 +48,21 @@ public class main_menu {
 
                             }
                             else if (submainChoice == '1') {
-                                System.out.println("Baza danych gier");
-                                //tutaj wywolac liste gier z klasy export_data
+                                System.out.println("Lista gier");
+                                //tutaj wywolac liste gier 1z klasy export_data
                             }
                             else if (submainChoice == '2') {
-                                System.out.println("Baza danych uzytkownikow");
+                                System.out.println("Lista uzytkownikow");
                                 //tutaj wywolac lise uzytkownikow z wykorzystaniem klasy export_data
+
+                                System.out.println();
                             }
                             else System.out.println("--Nieprawidlowe wskazanie. Wybierz ponownie.--");
             }
 
-            //Dodawanie wpisu do bazdy danych
+            //Dodawanie wpisu do list
             else if (mainChoice == '2') {
-
+                System.out.println();
                 System.out.println("Podaj liste ktora chcesz uzupelnic:");
                 System.out.println("    Wybierz (1) - lista gier");
                 System.out.println("    Wybierz (2) - lista uzytkownikow");
@@ -76,6 +80,7 @@ public class main_menu {
                                 //tutaj wywolac mozliwosc dodania gry z klasy import_data
 
                             } else if (submainChoice == '2') {
+
                                 System.out.println("Dodaj uzytkownika wg. nastepujace schematu: Imie, Nazwisko, adres e-mail");
                                 //tutaj wywolac mozliwosc dodania uzytkownika
                             }
